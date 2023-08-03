b475f2bccf727e76df2ef1625e9961a2e696a37f
Desumai <dustin.siu@gmail.com>
2023-08-03 14:37:02 -0400

    Cleanup fix

--- pdf_stuff.py
+++ pdf_stuff.py
@@ -1,13 +1,9 @@
-import pdf2image, fitz
+import fitz
 from fitz import Pixmap
 import numpy as np
 from PIL import Image
 from cv2 import imdecode
 
-def convert_pdf_to_image_list(filePath: str,popplerPath:str):
-    return pdf2image.convert_from_path(
-        filePath, poppler_path=popplerPath, fmt="png", dpi=300, thread_count=10
-    )
 
 def pdf_to_np_array_list(filePath:str):
     imgList = []
@@ -17,15 +13,3 @@ def pdf_to_np_array_list(filePath:str):
             npArr = imdecode(np.frombuffer(pixmap.tobytes(), dtype=np.uint8), -1)
             imgList.append(npArr)
     return imgList
-
-def save_temp_imgs(images:list, tempPath:str):
-    imgDict = {}
-    for i, image in enumerate(images):
-        path = tempPath + "\\" + "{:03d}".format(i) + ".png"
-        image.save(path, "png")
-        imgDict[i] = path
-    return imgDict
-
-def save_temp_imgs_from_pdf(images:list, filePath: str, popplerPath: str, tempPath:str = "temp"):
-    imageList = convert_pdf_to_image_list(filePath=filePath,popplerPath=popplerPath)
-    save_temp_imgs(images=imageList)
